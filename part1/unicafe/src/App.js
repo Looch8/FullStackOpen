c154717522fb1098cdcf95060411b6180944fa51
Luke <lukewheldale@gmail.com>
2023-01-17 21:56:57 +1030

    finished step 1 of unicafe exercise, added buttons and statics to sotre the button clicks

--- part1/unicafe/src/App.js
+++ part1/unicafe/src/App.js
@@ -1,16 +1,33 @@
-import {useState} from 'react'
+import { useState } from "react";
+
+const Display = (props) => (
+  <div>
+    {props.text} {props.score}
+  </div>
+);
+
+const Button = (props) => (
+  <button onClick={props.handleClick}>{props.text}</button>
+);
 
 const App = () => {
   //Save clicks of each button to its own state
-  const [good, setGood] = useState(0)
-  const [neutral, setNeutral] = useState(0)
-  const [bad, setBad] = useState(0)
+  const [good, setGood] = useState(0);
+  const [neutral, setNeutral] = useState(0);
+  const [bad, setBad] = useState(0);
 
   return (
     <div>
-      code here
-      </div>
-  )
-}
+      <h1>give feedback</h1>
+      <Button handleClick={() => setGood(good + 1)} text="good" />{" "}
+      <Button handleClick={() => setNeutral(neutral + 1)} text="neutral" />{" "}
+      <Button handleClick={() => setBad(bad + 1)} text="bad" />
+      <h1>statistics</h1>
+      <Display text="good" score={good} />
+      <Display text="neutral" score={neutral} />
+      <Display text="bad" score={bad} />
+    </div>
+  );
+};
 
 export default App;
