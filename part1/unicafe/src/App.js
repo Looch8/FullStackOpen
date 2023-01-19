8e66d560c4ea5520e348ae78224b0021312c46f4
Luke <lukewheldale@gmail.com>
2023-01-19 15:22:42 +1030

    finished unicafe exercise

--- part1/unicafe/src/App.js
+++ part1/unicafe/src/App.js
@@ -1,36 +1,63 @@
 import { useState } from "react";
 
-const Display = (props) => (
-  <div>
-    {props.text} {props.score}
-  </div>
-);
-
 const Button = (props) => (
   <button onClick={props.handleClick}>{props.text}</button>
 );
+
+const StatisticsText = (props) => (
+  <div>
+    {props.text}
+    {props.value}
+  </div>
+);
+
+const Statistics = ({ good, neutral, bad }) => {
+  const calcAverage = (good, neutral, bad) =>
+    (good - bad) / (good + bad + neutral).toFixed(2);
+
+  const calcPositive = (good, neutral, bad) =>
+    ((good * 100) / (good + neutral + bad)).toFixed(2) + "%";
+
+  return (
+    <>
+      <h1>Statistics</h1>
+      <table>
+        <tbody>
+          <StatisticsText text="good: " value={good} />{" "}
+          <StatisticsText text="neutral: " value={neutral} />
+          <StatisticsText text="bad: " value={bad} />
+          <StatisticsText text="total: " value={good + neutral + bad} />
+          <StatisticsText
+            text="average: "
+            value={
+              good + neutral + bad !== 0 ? calcAverage(good, neutral, bad) : 0
+            }
+          />
+          <StatisticsText
+            text="posititve: "
+            value={
+              good + neutral + bad !== 0 ? calcPositive(good, neutral, bad) : 0
+            }
+          />
+        </tbody>
+      </table>
+    </>
+  );
+};
 
 const App = () => {
   //Save clicks of each button to its own state
   const [good, setGood] = useState(0);
   const [neutral, setNeutral] = useState(0);
   const [bad, setBad] = useState(0);
- 
-  
- 
+
   return (
     <div>
       <h1>give feedback</h1>
       <Button handleClick={() => setGood(good + 1)} text="good" />{" "}
       <Button handleClick={() => setNeutral(neutral + 1)} text="neutral" />{" "}
       <Button handleClick={() => setBad(bad + 1)} text="bad" />
-      <h1>statistics</h1>
-      <Display text="good" score={good} />
-      <Display text="neutral" score={neutral} />
-      <Display text="bad" score={bad} />
-      <Display text='total' score={good + neutral + bad} />
-      <Display text='average' score ={(good - bad) / (good + bad + neutral)} />
-      <Display text='positive' score={(good * 100) / (good + neutral + bad) + '%'} />
+      <Statistics good={good} neutral={neutral} bad={bad} />
     </div>
   );
 };
