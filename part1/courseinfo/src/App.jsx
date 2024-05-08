bc5a8a0ae74608bb0f8ec50cb2a47b4f9a462e8d
Luke Wheldale <lukewheldale@gmail.com>
2024-05-08 21:44:57 +0930

    objects

--- part1/courseinfo/src/App.jsx
+++ part1/courseinfo/src/App.jsx
@@ -2,14 +2,10 @@ const Header = ({ course }) => {
 	return <h1>{course}</h1>;
 };
 
-// Refactor the Content component so that it does not render any names of parts or their number of exercises by itself. Instead, it only renders three Part components of which each renders the name and number of exercises of one part.
-
-const Content = ({ part1, exercise1, part2, exercise2, part3, exercise3 }) => {
+const Content = ({ part }) => {
 	return (
 		<div>
-			<Part part1={part1} exercise1={exercise1} />
-			<Part part1={part2} exercise1={exercise2} />
-			<Part part1={part3} exercise1={exercise3} />
+			<Part part={part} />
 		</div>
 	);
 };
@@ -28,25 +24,23 @@ const Total = ({ total }) => {
 
 const App = () => {
 	const course = "Half Stack application development";
-	const part1 = "Fundamentals of React";
-	const exercises1 = 10;
-	const part2 = "Using props to pass data";
-	const exercises2 = 7;
-	const part3 = "State of a component";
-	const exercises3 = 14;
+	const part1 = {
+		name: "Fundamentals of React",
+		exercises: 10,
+	};
+	const part2 = {
+		name: "Using props to pass data",
+		exercises: 7,
+	};
+	const part3 = {
+		name: "State of a component",
+		exercises: 14,
+	};
 
 	return (
 		<div>
 			<Header course={course} />
-			<Content
-				part1={part1}
-				exercise1={exercises1}
-				part2={part2}
-				exercise2={exercises2}
-				part3={part3}
-				exercise3={exercises3}
-			/>
-			<Total total={exercises1 + exercises2 + exercises3} />
+			<Content part={part1.name} />
 		</div>
 	);
 };
