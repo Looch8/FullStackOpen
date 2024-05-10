33acca5b1ef3e364f45be9dc2b3e055abc20be0e
Luke Wheldale <lukewheldale@gmail.com>
2024-05-10 21:58:14 +0930

    step 3

--- part1/unicafe/src/App.jsx
+++ part1/unicafe/src/App.jsx
@@ -16,19 +16,30 @@ const App = () => {
 			<button onClick={() => setGood(good + 1)}>good</button>
 			<button onClick={() => setNeutral(neutral + 1)}>neutral</button>
 			<button onClick={() => setBad(bad + 1)}>bad</button>
-			<h2>statistics</h2>
-			<p>good {good}</p>
-			<p>neutral {neutral}</p>
-			<p>bad {bad}</p>
-			<p>all {all}</p>
-			<p>average {average}</p>
-			<p>positive {positive}</p>
+			<Statistics
+				good={good}
+				neutral={neutral}
+				bad={bad}
+				all={all}
+				average={average}
+				positive={positive}
+			/>
 		</div>
 	);
 };
 
 export default App;
 
-const Button = ({ text }) => {
-	return <button>{text}</button>;
+const Statistics = ({ good, neutral, bad, all, average, positive }) => {
+	return (
+		<div>
+			<h2>statistics</h2>
+			<p>good {good}</p>
+			<p>neutral {neutral}</p>
+			<p>bad {bad}</p>
+			<p>all {all}</p>
+			<p>average {average}</p>
+			<p>positive {positive ? positive : 0}</p>
+		</div>
+	);
 };
