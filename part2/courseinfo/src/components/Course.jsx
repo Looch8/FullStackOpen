65f2ddcb8d16419f79e3688fa7f4f323436b92dd
Luke Wheldale <lukewheldale@gmail.com>
2024-05-12 20:31:20 +0930

    course info step 10

---
+++ part2/courseinfo/src/components/Course.jsx
@@ -0,0 +1,30 @@
+const Course = ({ course }) => {
+	return (
+		<>
+			<Header course={course.name} />
+			<Content parts={course.parts} />
+			<Total
+				sum={course.parts.reduce(
+					(acc, part) => acc + part.exercises,
+					0
+				)}
+			/>
+		</>
+	);
+};
+
+const Header = ({ course }) => <h1>{course}</h1>;
+
+const Total = ({ sum }) => <p>Number of exercises {sum}</p>;
+
+const Part = ({ part }) => (
+	<p>
+		{part.name} {part.exercises}
+	</p>
+);
+
+const Content = ({ parts }) => (
+	<>{parts.map((part) => (part = <Part key={part.id} part={part} />))}</>
+);
+
+export default Course;
