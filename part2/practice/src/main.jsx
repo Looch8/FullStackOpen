15454bc8adb516f5a6d8b181791f5a32f0cdcf8b
Luke Wheldale <lukewheldale@gmail.com>
2024-06-11 16:39:23 +0930

    set up axios

--- part2/practice/src/main.jsx
+++ part2/practice/src/main.jsx
@@ -2,6 +2,15 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 
 import App from "./App";
+import axios from "axios";
+
+axios.get("http://localhost:3001/notes").then((response) => {
+	const notes = response.data;
+	console.log(notes);
+});
+
+const promise2 = axios.get("http://localhost:3001/foobar");
+console.log(promise2);
 
 const notes = [
 	{
