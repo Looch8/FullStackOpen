6ee89943a31ce11a5cb06ba1a3f8470be3c85ba8
Luke Wheldale <lukewheldale@gmail.com>
2024-08-22 20:24:53 +0930

    course info 1.2

--- part1/courseinfo/src/App.jsx
+++ part1/courseinfo/src/App.jsx
@@ -10,28 +10,44 @@ const App = () => {
 	return (
 		<div>
 			<Header course={course} />
-			<Content part={part1} exercise={exercises1} />
-			<Content part={part2} exercise={exercises2} />
-			<Content part={part3} exercise={exercises3} />
+			<Content
+				part1={part1}
+				exercise1={exercises1}
+				part2={part2}
+				exercise2={exercises2}
+				part3={part3}
+				exercise3={exercises3}
+			/>
+
 			<Total total={exercises1 + exercises2 + exercises3} />
 		</div>
 	);
 };
 
-const Header = (props) => {
-	return <h1>{props.course}</h1>;
+const Header = ({ course }) => {
+	return <h1>{course}</h1>;
 };
 
-const Content = (props) => {
+const Content = ({ part1, exercise1, part2, exercise2, part3, exercise3 }) => {
+	return (
+		<div>
+			<Part part={part1} exercise={exercise1} />
+			<Part part={part2} exercise={exercise2} />
+			<Part part={part3} exercise={exercise3} />
+		</div>
+	);
+};
+
+const Part = ({ part, exercise }) => {
 	return (
 		<p>
-			{props.part} {props.exercise}
+			{part} {exercise}
 		</p>
 	);
 };
 
-const Total = (props) => {
-	return <p>Number of exercises {props.total}</p>;
+const Total = ({ total }) => {
+	return <p>Number of exercises {total}</p>;
 };
 
 export default App;
