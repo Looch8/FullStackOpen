53ec4440732b7f091e1866f85de2e2bb4c640c4c
Luke Wheldale <lukewheldale@gmail.com>
2024-08-29 12:53:22 +0930

    phonebook step 1 - add names to list

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -4,19 +4,40 @@ const App = () => {
 	const [persons, setPersons] = useState([{ name: "Arto Hellas" }]);
 	const [newName, setNewName] = useState("");
 
+	const addName = (event) => {
+		event.preventDefault();
+		const nameObject = {
+			name: newName,
+		};
+
+		setPersons(persons.concat(nameObject));
+		setNewName("");
+	};
+
+	const handleNameChange = (event) => {
+		setNewName(event.target.value);
+	};
+
 	return (
 		<div>
 			<h2>Phonebook</h2>
-			<form>
+			<form onSubmit={addName}>
 				<div>
-					name: <input />
+					name:{" "}
+					<input
+						placeholder="Add name..."
+						value={newName}
+						onChange={handleNameChange}
+					/>
 				</div>
 				<div>
-					<button type="submit">add</button>
+					<button type="submit">add </button>
 				</div>
 			</form>
-			<h2>Numbers</h2>
-			...
+			<h2>Numbers</h2>{" "}
+			{persons.map((person) => (
+				<div key={person.name}>{person.name}</div>
+			))}
 		</div>
 	);
 };
